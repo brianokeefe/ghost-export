f062f8a63ead88b724170c0be05521d7a8b4b90d
Brian O'Keefe <brian@bokstuff.com>
2014-06-08 16:32:22 -0400

    Use Grunt and Jasmine

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jasmine_node: {
+      options: {
+        forceExit: true,
+        match: '.',
+        matchall: false,
+        extensions: 'js',
+        specNameMatcher: 'spec',
+        jUnit: {
+          report: true,
+          savePath : "./build/reports/jasmine/",
+          useDotNotation: true,
+          consolidate: true
+        }
+      },
+      all: ['spec/']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-jasmine-node');
+
+  grunt.registerTask('default', ['jasmine_node']);
+}
---
+++ spec/ghost-export-spec.js
@@ -0,0 +1,1 @@
+var exporter = require('../lib/ghost-export');
